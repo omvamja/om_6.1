3005fd18652d4b37f257edc3e3ea531d1b6bf981
omvamja <136708203+omvamja@users.noreply.github.com>
2023-06-28 21:43:53 +0530

    Add files via upload

---
+++ OM61.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main (){
+int a=1;
+int n;
+clrscr();
+
+printf("enter your number");
+scanf("%d",&n);
+
+do{
+	printf("%d\n",n);
+	n--;
+	}while (a<=n);
+
+
+	getch();}
